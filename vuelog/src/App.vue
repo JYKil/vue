7657e92eec9ddc5305186fcc770676e8fb8fb258
JYKil <kilga2402@gmail.com>
2022-11-11 12:51:41 +0900

    add blog data & install router

--- vuelog/src/App.vue
+++ vuelog/src/App.vue
@@ -1,17 +1,61 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <nav class="navbar navbar-expand-lg bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">VueLog</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Features</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Pricing</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled">Disabled</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <div class="container mt-4">
+    <h5>Routing 테스트를 위한 개발자 블로그</h5>
+    <p>- Vue로 만들었음 -</p>
+  </div>
+
+  <List :블로그글="블로그글"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import List from './components/List.vue';
+import blog from './assets/blog.js'
 
 export default {
-  name: 'App',
+  name: "App",
+  // 원래 blog.js 내용을 넣을려다가 import 함
+  data(){
+    return{
+      블로그글: blog
+    }
+  }, 
   components: {
-    HelloWorld
-  }
-}
+    List : List,
+  },
+};
 </script>
 
 <style>
@@ -21,6 +65,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
